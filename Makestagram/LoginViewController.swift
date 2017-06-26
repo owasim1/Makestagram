6a3c4cba5af6f20dcc30d3f8afca1a913c8c009d
owasim1 <omar.wasim@valpo.edu>
2017-06-26 14:23:46 -0700

    initial commit 2

---
+++ Makestagram/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  Makestagram
+//
+//  Created by Omar Wasim on 6/26/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Do any additional setup after loading the view
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //Dispose of any resources that can be recreated.
+    }
+    
+    
+}
