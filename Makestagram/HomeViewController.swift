e915ea79845beaeb693114fcb42e439d6911c011
owasim1 <omar.wasim@valpo.edu>
2017-07-04 10:56:45 -0700

    Step 18 start

--- Makestagram/HomeViewController.swift
+++ Makestagram/HomeViewController.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import Kingfisher
 
 class HomeViewController: UIViewController {
     
@@ -17,11 +18,19 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad(){
         super.viewDidLoad()
+        configureTableView()
         
         UserService.posts(for: User.current) { (posts) in
             self.posts = posts
             self.tableView.reloadData()
         }
+    }
+    
+    func configureTableView() {
+        // remove separators for empty cells
+        tableView.tableFooterView = UIView()
+        // remove separators from cells
+        tableView.separatorStyle = .none
     }
 }
 
@@ -38,8 +47,11 @@ extension HomeViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PostImageCell", for: indexPath)
-        cell.backgroundColor = .red
+        let post = posts[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostImageCell", for: indexPath) as! PostImageCell
+        
+        let imageURL = URL(string: post.imageURL)
+        cell.postImageView.kf.setImage(with: imageURL)
         
         return cell
     }
---
+++ Makestagram/PostImageCell.swift
@@ -0,0 +1,19 @@
+//
+//  PostImageCell.swift
+//  Makestagram
+//
+//  Created by Omar Wasim on 7/2/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PostImageCell: UITableViewCell {
+    
+    @IBOutlet weak var postImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
